4dbd2ed3e3f8516b839568f706c3ba3aa9812cb0
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-11-05 16:44:42 +0000

    start my server and test it

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+const port = process.env.PORT || 8008;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my now server on port ' + port)
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
