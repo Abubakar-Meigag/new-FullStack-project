d2cf3e27ebdaed4a03ecfee2b46b617d7777f78e
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-11-05 21:13:19 +0000

    update

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my now server on port ' + port)
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
 
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.get('/', (req, res) => {
+    res.send('Welcome to my now server on port' + port)
 });
